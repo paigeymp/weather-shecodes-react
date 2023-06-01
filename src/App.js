232b7dc7379f7c71dffc67cb7b2fbf68e05376c4
Paige Powell <117302610+paigeymp@users.noreply.github.com>
2023-06-01 15:29:22 -0400

    Added link you GitHub repo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        <p>
+          This project is created by Paige Powell and is{" "}
+          <a
+            href="https://github.com/paigeymp/weather-shecodes-react"
+            target="_blank"
+            title="github"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </p>
+      </footer>
+    </div>
+  );
 }
