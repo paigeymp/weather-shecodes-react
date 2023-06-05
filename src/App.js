faa8a370f408ace034cd6e82f4e6151da112a89e
Paige Powell <117302610+paigeymp@users.noreply.github.com>
2023-06-05 13:59:50 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import React from "react";
 import "./App.css";
 import Weather from "./Weather";
 
 export default function App() {
   return (
-    <div className="container">
-      <div className="App">
+    <div className="App">
+      <div className="container">
         <Weather defaultCity="London" />
         <footer>
           This project is created by Paige Powell and is{" "}
